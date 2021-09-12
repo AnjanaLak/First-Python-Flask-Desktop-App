d96c4b6eaefd825183950603fd17134ac2c2a483
AnjanaLak <hkanjanalakshan95@gmail.com>
2021-09-13 01:47:13 +0530

    completed everything for create and view data

--- src/ui/app.js
+++ src/ui/app.js
@@ -2,22 +2,7 @@ const myform = document.getElementById('myform')
 const title = document.getElementById('title')
 const body = document.getElementById('description')
 
-
-// Adding event listeners
-
-myform.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    const newData = {
-        title: title.value,
-        body: body.value
-    }
-    
-    insertData(newData)
-
-})
-
-
+const articles = document.getElementById('articles')
 
 // Connecting with backend
 
@@ -30,8 +15,67 @@ const insertData = (newData) => {
         body:JSON.stringify(newData)
     })
     .then(resp => resp.json())
-    .then((data) => {
-        console.log(data)
+    // .then((data) => {
+    //     console.log(data)
+    // })
+    .then(() => {
+        getAllData()  //////////////////////// To GET ALL DATA AFTER INSERTING
     })
     .catch(error => console.log(error))
 }
+
+const getAllData = () => {
+    fetch('http://127.0.0.1:5000/get', {
+        method : 'GET',
+        headers : {
+            'Content-Type' : 'application/json'
+        }
+    })
+    .then(resp => resp.json())
+    // .then(data => console.log(data))
+    .then(data => renderArticles(data))
+    .catch(error => console.log(error))
+}
+
+// Rendering articles to the UI
+function renderArticles(mydata) {
+    articles.innerHTML = '';
+
+    mydata.forEach(data => {
+        articles.innerHTML += `
+        <div class = "card card-body my-2">
+        <h2>${data.title}</h2>
+        <p>${data.body}</p>
+        <h5>${data.date}</h5>
+
+        <p>
+        <button class="btn btn-danger">Delete</button>
+        <button class="btn btn-success">Update</button>
+        </p>
+
+        </div>
+        `
+    })
+
+}
+
+// Adding event listeners
+
+myform.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const newData = {
+        title: title.value,
+        body: body.value
+    }
+    
+    insertData(newData)
+    myform.reset() // to reset, otherwise it will not show data in the UI after added
+
+})
+
+// Classes, functions like variables declared with const and let, cannot be referenced before the line that initializes
+// them runs. For example, the following is forbidden:
+// console.log(foo);
+// const foo = 'foo';
+getAllData()   // calling the get method
