38eabad0dcc3a2b55d3ca95ea0939b427d3596de
AnjanaLak <hkanjanalakshan95@gmail.com>
2021-09-12 20:07:51 +0530

    get and post methods implemented

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask.json import jsonify
 from flask_sqlalchemy import SQLAlchemy
 import datetime
@@ -12,19 +12,41 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-class Atricles(db.Model):
+class Articles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
-    description = db.Column(db.Text())
+    body = db.Column(db.Text())
     date = db.Column(db.DateTime, default = datetime.datetime.now)
 
     def __init__(self, title, body):
         self.title = title
         self.body = body
 
-@app.route('/', methods = ['GET'])
+class ArticleSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'body', 'date')
+
+article_schema = ArticleSchema()
+articles_schema = ArticleSchema(many=True)
+
+
+@app.route('/get', methods = ['GET'])
 def get_articles():
-    return jsonify({"Hello":"World"})
+    # return jsonify({"Hello":"World"})
+    all_articles = Articles.query.all()
+    results = articles_schema.dump(all_articles)  # note that we are using 's' as it will get many objects
+    return jsonify(results)
+
+@app.route('/add', methods = ['POST'])
+def add_articles():
+    title = request.json['title']
+    body = request.json['body']
+
+    articles = Articles(title, body) # creating an article object
+    db.session.add(articles)  # adding the record to the object
+    db.session.commit()
+    return article_schema.jsonify(articles)
+
 
 
 if __name__ == "__main__":
