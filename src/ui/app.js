4160812dda9d780bf54d29d2f133e3aa0e1b6c9b
AnjanaLak <hkanjanalakshan95@gmail.com>
2021-09-13 06:21:29 +0530

    Application Finalized

--- src/ui/app.js
+++ src/ui/app.js
@@ -4,6 +4,8 @@ const body = document.getElementById('description')
 
 const articles = document.getElementById('articles')
 
+let articleId = 0;
+
 // Connecting with backend
 
 const insertData = (newData) => {
@@ -37,6 +39,59 @@ const getAllData = () => {
     .catch(error => console.log(error))
 }
 
+const deleteData = (id) => {
+    fetch(`http://127.0.0.1:5000/delete/${id}/`, {
+        method : 'DELETE',
+        headers : {
+            'Content-Type' : 'application/json'
+        }
+        
+    })
+    getAllData() // to reload the data
+}
+
+// the update process ===> SOmething interesting
+
+const getDataById = (id) => {
+    fetch(`http://127.0.0.1:5000/get/${id}/`, {
+        method : 'GET',
+        headers : {
+            'Content-Type' : 'application/json'
+        }
+        
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        // console.log(data['title']);
+        renderOneItem(data)
+    })
+    getAllData() // to reload the data
+}
+
+const renderOneItem = (mydata) => {
+    title.value = mydata.title
+    body.value = mydata.body
+
+    articleId = mydata.id
+}
+
+const updateData = (articleId, mydata) => {
+    fetch(`http://127.0.0.1:5000/update/${articleId}/`, {
+        method : 'PUT',
+        headers : {
+            'Content-Type' : 'application/json'
+        },
+        body:JSON.stringify(mydata)
+        
+    })
+    .then(resp => resp.json())
+    .then(() => {
+        getAllData() 
+    })
+}
+ 
+
+
 // Rendering articles to the UI
 function renderArticles(mydata) {
     articles.innerHTML = '';
@@ -49,8 +104,8 @@ function renderArticles(mydata) {
         <h5>${data.date}</h5>
 
         <p>
-        <button class="btn btn-danger">Delete</button>
-        <button class="btn btn-success">Update</button>
+        <button class="btn btn-danger" onclick="deleteData('${data.id}')">Delete</button>
+        <button class="btn btn-success" onclick="getDataById('${data.id}')")>Update</button>
         </p>
 
         </div>
@@ -58,6 +113,8 @@ function renderArticles(mydata) {
     })
 
 }
+
+
 
 // Adding event listeners
 
@@ -68,8 +125,17 @@ myform.addEventListener('submit', (e) => {
         title: title.value,
         body: body.value
     }
+
+    // to check wheteher an id is available
+    if(articleId != 0){
+        updateData(articleId, newData)
+        articleId = 0
+    }
+    // else we simply insert data
+    else {
+        insertData(newData)
+    }
     
-    insertData(newData)
     myform.reset() // to reset, otherwise it will not show data in the UI after added
 
 })
